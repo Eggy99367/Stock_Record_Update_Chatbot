6a2074c654d5577afd7567122d351e5f0862befe
Vincent Chen <yinhsuac@uci.edu>
2025-09-06 18:05:30 +0800

    integrated services and implemented basic add_record_flow

--- app.py
+++ app.py
@@ -4,20 +4,37 @@ import os
 import json
 from google.oauth2.service_account import Credentials
 
-from linebot import LineBotApi
-from linebot.v3.webhook import WebhookHandler
-from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage, TemplateSendMessage, ButtonsTemplate, MessageAction
-from src.service import Create_Service
+# from linebot import LineBotApi, WebhookHandler
+# from linebot.exceptions import InvalidSignatureError
+# from linebot.models import TextSendMessage, TemplateSendMessage, ButtonsTemplate, MessageAction
 from src.sheet_operations import SheetOperations
-
+from linebot.v3 import (
+    WebhookHandler
+)
+from linebot.v3.exceptions import (
+    InvalidSignatureError
+)
+from linebot.v3.messaging import (
+    Configuration,
+    ApiClient,
+    MessagingApi,
+    ReplyMessageRequest,
+    TextMessage,
+    TemplateMessage,
+    ButtonsTemplate,
+    MessageAction
+)
+from linebot.v3.webhooks import (
+    MessageEvent,
+    TextMessageContent
+)
 app = Flask(__name__)
 load_dotenv()
+# WEBHOOK_HANDLER = WebhookHandler(os.getenv("LINE_CHANNEL_SECRET"))
+# LINE_BOT_API = LineBotApi(os.getenv("LINE_CHANNEL_ACCESS_TOKEN"))
+CONFIGURATION = Configuration(access_token=os.getenv("LINE_CHANNEL_ACCESS_TOKEN"))
 WEBHOOK_HANDLER = WebhookHandler(os.getenv("LINE_CHANNEL_SECRET"))
-LINE_BOT_API = LineBotApi(os.getenv("LINE_CHANNEL_ACCESS_TOKEN"))
 
-# SS_SERVICE = Create_Service('sheets', 'v4', ['https://www.googleapis.com/auth/spreadsheets'])
-# DRIVE_SERVICE = Create_Service('drive', 'v3', ['https://www.googleapis.com/auth/drive'])
 sheet_ops = SheetOperations(os.environ.get("GOOGLE_SERVICE_ACCOUNT_JSON"))
 # load user data
 USER_DATA_FILE = 'user_data.json'
@@ -43,7 +60,7 @@ def callback():
         abort(400)
     return 'OK'
 
-@WEBHOOK_HANDLER.add(MessageEvent, message=TextMessage)
+@WEBHOOK_HANDLER.add(MessageEvent, message=TextMessageContent)
 def handle_message(event):
     message = event.message.text
     user_id = event.source.user_id
@@ -63,7 +80,7 @@ def handle_message(event):
         user_data[user_id]["state"] = "set_spreadsheet_id"
         message = "請輸入試算表ID"
     elif user_data[user_id]["state"] == "set_spreadsheet_id":
-        user_data[user_id]["sheet_id"] = message
+        user_data[user_id]["spreadsheet_id"] = message
         user_data[user_id]["state"] = "set_sheet_name"
         save_user_data()
         message = f"已設定試算表ID為: {message}\n\n請接著輸入工作表名稱"
@@ -73,44 +90,42 @@ def handle_message(event):
         save_user_data()
         message = f"已設定工作表名稱為: {message}\n\n可以開始更新了!"
     elif message.startswith("新增交易紀錄"):
-        if "sheet_id" not in user_data[user_id] or "sheet_name" not in user_data[user_id]:
+        if "spreadsheet_id" not in user_data[user_id] or "sheet_name" not in user_data[user_id]:
             message = "請先設定試算表ID和工作表名稱，輸入「設定試算表」開始設定"
         else:
             user_data[user_id]["state"] = "add_trade_record"
             save_user_data()
             message = ("請輸入股票代碼:")
     elif user_data[user_id]["state"] == "add_trade_record":
-        user_data[user_id]["trade_record"] = {"code": message}
-        user_data[user_id]["state"] = "set_trade_option"
-        save_user_data()
-        message = TemplateSendMessage(
-            alt_text="選擇交易選項",
-            template=ButtonsTemplate(
-                text = "請選擇交易選項: [買入/賣出]",
-                actions = [
-                    MessageAction(
-                        label = "買入",
-                        text = "買入"
-                    ),
-                    MessageAction(
-                        label = "賣出",
-                        text = "賣出"
-                    )
-                ]
+        if sheet_ops.row_of_ticker_symbol(user_data[user_id]["spreadsheet_id"], user_data[user_id]["sheet_name"], message) == -1:
+            message = "表單中找不到股票代碼，請重新輸入股票代碼:\n(請確認是否已在試算表中新增該股票代碼)"
+        else:
+            user_data[user_id]["trade_record"] = {"code": message.strip().upper()}
+            user_data[user_id]["state"] = "set_trade_option"
+            save_user_data()
+
+            message = TemplateMessage(
+                alt_text="選擇交易選項",
+                template=ButtonsTemplate(
+                    text="請選擇交易選項: [買入/賣出]",
+                    actions=[
+                        MessageAction(label="買入", text="買入"),
+                        MessageAction(label="賣出", text="賣出")
+                    ]
+                )
             )
-        )
     elif user_data[user_id]["state"] == "set_trade_option":
         if message not in ["買入", "賣出"]:
             message = "交易選項錯誤，請輸入「買入」或「賣出」"
         else:
-            user_data[user_id]["trade_record"]["option"] = message
+            user_data[user_id]["trade_record"]["option"] = "+" if message == "買入" else "-"
             user_data[user_id]["state"] = "set_trade_quantity"
             message = "請輸入交易數量:"
             save_user_data()
     elif user_data[user_id]["state"] == "set_trade_quantity":
         try:
             quantity = int(message)
-            if quantity < 0:
+            if quantity <= 0:
                 raise ValueError
             user_data[user_id]["trade_record"]["quantity"] = quantity
             user_data[user_id]["state"] = "set_trade_price"
@@ -120,8 +135,8 @@ def handle_message(event):
             message = "交易數量錯誤，請輸入正整數"
     elif user_data[user_id]["state"] == "set_trade_price":
         try:
-            price = float(message)
-            if price <= 0:
+            price = round(float(message), 2)
+            if price < 0:
                 raise ValueError
             user_data[user_id]["trade_record"]["price"] = price
             record = user_data[user_id]["trade_record"]
@@ -130,48 +145,67 @@ def handle_message(event):
 
             message = (f"已新增交易紀錄:\n股票代碼: {record['code']}\n交易選項: {record['option']}\n"
                           f"交易數量: {record['quantity']}\n交易價格: {record['price']}")
-            message = TemplateSendMessage(
-            alt_text="確認交易紀錄",
-            template=ButtonsTemplate(
-                text = "請確認交易紀錄是否正確:\n"
-                        f"股票代碼: {record['code']}\n"
-                        f"交易選項: {record['option']}\n"
-                        f"交易數量: {record['quantity']}\n"
-                        f"交易價格: {record['price']}",
-                actions = [
-                    MessageAction(
-                        label = "確認",
-                        text = "確認"
-                    ),
-                    MessageAction(
-                        label = "取消",
-                        text = "取消"
-                    )
-                ]
+            message = TemplateMessage(
+                alt_text="確認交易紀錄",
+                template=ButtonsTemplate(
+                    text="請確認交易紀錄是否正確:\n\n"
+                            f"[{record['code']}] {'買入' if record['option'] == '+' else '賣出'} {record['option']}{record['quantity']} @${record['price']:.2f}",
+                    actions=[
+                        MessageAction(
+                            label="確認",
+                            text="確認"
+                        ),
+                        MessageAction(
+                            label="取消",
+                            text="取消"
+                        )
+                    ]
+                )
             )
-        )
             user_data[user_id]["state"] = "confirm_trade"
-            user_data[user_id].pop("trade_record", None)
+            # user_data[user_id].pop("trade_record", None)
             save_user_data()
         except ValueError:
             message = "交易價格錯誤，請輸入正數"
+    elif message == "確認":
+        if sheet_ops.row_of_ticker_symbol(user_data[user_id]["spreadsheet_id"], user_data[user_id]["sheet_name"], user_data[user_id]["trade_record"]["code"]) == -1:
+            message = "表單中找不到股票代碼，請重新輸入股票代碼:\n(請確認是否已在試算表中新增該股票代碼)"
+        else:
+            if "sheet_ids" not in user_data[user_id]:
+                user_data[user_id]["sheet_ids"] = {}
+                save_user_data()
+            if "登錄交易紀錄" not in user_data[user_id]["sheet_ids"]:
+                sheet_id = sheet_ops.get_sheet_id(user_data[user_id]["spreadsheet_id"], "登錄交易紀錄")
+                if sheet_id is not None:
+                    user_data[user_id]["sheet_ids"]["登錄交易紀錄"] = sheet_id
+                    save_user_data()
+            sheet_ops.insert_row(user_data[user_id]["spreadsheet_id"], user_data[user_id]["sheet_ids"]["登錄交易紀錄"], 3)
+            print("row inserted")
+            sheet_ops.copy_range(user_data[user_id]["spreadsheet_id"], user_data[user_id]["sheet_ids"]["登錄交易紀錄"], (1, 0), (2, 10), (2, 0))
+
+            sheet_ops.add_trade_record(user_data[user_id]["spreadsheet_id"], user_data[user_id]["sheet_name"], "登錄交易紀錄", user_data[user_id]["trade_record"])
 
     # cmd = message.split()[0]
     reply_msg(event.reply_token, message)
 
 def reply_msg(reply_token, message):
-    if type(message) == str:
-        LINE_BOT_API.reply_message(reply_token, TextSendMessage(text=message))
-    else:
-        LINE_BOT_API.reply_message(reply_token, message)
-
+    with ApiClient(CONFIGURATION) as api_client:
+        line_bot_api = MessagingApi(api_client)
+        if isinstance(message, str):
+            messages = [TextMessage(text=message)]
+        else:
+            messages = [message]
+            
+        line_bot_api.reply_message_with_http_info(
+            ReplyMessageRequest(
+                reply_token=reply_token,
+                messages=messages
+            )
+        )
 
 
 
 
 if __name__ == "__main__":
-    print(sheet_ops.check_sheet_exist('1BIo33P4uYqxXZRwuktu2dQObOYDhOLo-ACjRfVAapBk', '登錄交易紀錄'))
-    print(sheet_ops.check_sheet_exist('1BIo33P4uYqxXZRwuktu2dQObOYDhOLo-ACjRfVAapBk', '出入金紀錄'))
-    # app.run()
-    # edit_cell('1BIo33P4uYqxXZRwuktu2dQObOYDhOLo-ACjRfVAapBk', 'Vincent', (0, 0), "TEST")
+    app.run()
     pass
--- src/sheet_operations.py
+++ src/sheet_operations.py
@@ -1,6 +1,22 @@
-from .service import Create_Service
 import os
 import json
+import re
+import time
+from googleapiclient.discovery import build
+from google.oauth2 import service_account
+
+def Create_Service(client_secret, api_name, api_version, *scopes):
+    SCOPES = [scope for scope in scopes[0]]
+    cred = service_account.Credentials.from_service_account_file(
+        client_secret, scopes=SCOPES
+    )
+    try:
+        service = build(api_name, api_version, credentials=cred)
+        return service
+    except Exception as e:
+        print('Unable to connect', api_name, api_version, scopes, sep='-')
+        print(e)
+        return None
 
 class SheetOperations:
     def __init__(self, client_secret):
@@ -11,10 +27,10 @@ class SheetOperations:
         self.drive_service = Create_Service("google_service_account.json", 'drive', 'v3', ['https://www.googleapis.com/auth/drive'])
         os.remove("google_service_account.json")
 
-    def check_sheet_exist(self, sheet_id, sheet_name):
+    def check_sheet_exist(self, spreadsheet_id, sheet_name):
         try:
             sheet = self.ss_service.spreadsheets().get(
-                spreadsheetId=sheet_id,
+                spreadsheetId=spreadsheet_id,
                 ranges=sheet_name
             ).execute()
             return True
@@ -22,15 +38,155 @@ class SheetOperations:
             # print(f"Error checking sheet existence: {e}")
             return False
 
-    def edit_cell(self, sheet_id, ss_name, index, new_value):
+    def get_sheet_id(self, spreadsheet_id, sheet_name):
+        spreadsheet = self.ss_service.spreadsheets().get(spreadsheetId=spreadsheet_id).execute()
+        for s in spreadsheet.get("sheets", []):
+            if s["properties"]["title"] == sheet_name:
+                return s["properties"]["sheetId"]
+        return None
+
+    def insert_row(self, spreadsheet_id, sheet_id, row_number):
+        request_body = {
+            "requests": [
+                {
+                    "insertDimension": {
+                        "range": {
+                            "sheetId": sheet_id,
+                            "dimension": "ROWS",
+                            "startIndex": row_number - 1,  # 0-based index
+                            "endIndex": row_number
+                        },
+                        "inheritFromBefore": True
+                    }
+                }
+            ]
+        }
+
+        self.ss_service.spreadsheets().batchUpdate(
+            spreadsheetId=spreadsheet_id,
+            body=request_body
+        ).execute()
+
+    def edit_cell(self, spreadsheet_id, ss_name, index, new_value):
         range_name = f'{ss_name}!{chr(65 + index[1])}{index[0] + 1}'
-        print(range_name)
+        # print(range_name)
         request_body = {
             'values': [[new_value]]
         }
         response = self.ss_service.spreadsheets().values().update(
-            spreadsheetId=sheet_id,
+            spreadsheetId=spreadsheet_id,
             range=range_name,
             valueInputOption='RAW',
             body=request_body
         ).execute()
+
+    def edit_range(self, spreadsheet_id, ss_name, start_index, end_index, new_values):
+        range_name = f'{ss_name}!{chr(65 + start_index[1])}{start_index[0] + 1}:{chr(65 + end_index[1])}{end_index[0] + 1}'
+        request_body = {
+            'values': new_values
+        }
+        response = self.ss_service.spreadsheets().values().update(
+            spreadsheetId=spreadsheet_id,
+            range=range_name,
+            valueInputOption='RAW',
+            body=request_body
+        ).execute()
+
+    def get_range(self, spreadsheet_id, ss_name, start_index, end_index):
+        range_name = f'{ss_name}!{chr(65 + start_index[1])}{start_index[0] + 1}:{chr(65 + end_index[1])}{end_index[0] + 1}'
+
+        get_values = self.ss_service.spreadsheets().values().get(
+            spreadsheetId=spreadsheet_id,
+            majorDimension='ROWS',
+            range=range_name
+        ).execute()['values']
+
+        return get_values
+
+    def copy_range(self, spreadsheet_id, sheet_id,
+                    start, end, dest, paste_type="PASTE_NORMAL"):
+        start_row, start_col = start
+        end_row, end_col = end
+        dest_start_row, dest_start_col = dest
+        requests = [
+            {
+                "copyPaste": {
+                    "source": {
+                        "sheetId": sheet_id,
+                        "startRowIndex": start_row,
+                        "endRowIndex": end_row,
+                        "startColumnIndex": start_col,
+                        "endColumnIndex": end_col
+                    },
+                    "destination": {
+                        "sheetId": sheet_id,
+                        "startRowIndex": dest_start_row,
+                        "endRowIndex": dest_start_row + (end_row - start_row),
+                        "startColumnIndex": dest_start_col,
+                        "endColumnIndex": dest_start_col + (end_col - start_col)
+                    },
+                    "pasteType": paste_type
+                }
+            }
+        ]
+
+        body = {"requests": requests}
+        self.ss_service.spreadsheets().batchUpdate(
+            spreadsheetId=spreadsheet_id,
+            body=body
+        ).execute()
+
+        print(f"Copied range {start_row}-{end_row}, {start_col}-{end_col} to {dest_start_row}, {dest_start_col}")
+
+
+    def row_of_ticker_symbol(self, spreadsheet_id, sheet_name, ticker_symbol):
+        range_name = f'{sheet_name}!A:A'
+        result = self.ss_service.spreadsheets().values().get(
+            spreadsheetId=spreadsheet_id,
+            range=range_name
+        ).execute()
+        values = result.get('values', [])
+        for i, row in enumerate(values):
+            if not row or not row[0]:
+                continue
+            row = re.split(r'\W+', row[0])[0]
+            if row.upper() == ticker_symbol.strip().upper():
+                return i
+        return -1
+    
+    def add_trade_record(self, spreadsheet_id, overview_sheet_name, record_sheet_name, record):
+        print("Adding trade record:", record)
+        original_record = self.get_current_records(spreadsheet_id, overview_sheet_name, record["code"])
+        range_name = f'{record_sheet_name}!A:E'
+        original_amount  = float(original_record['股數']["value"]) if original_record else 0
+        new_amount = original_amount + record['quantity'] if record['option'] == '+' else original_amount - record['quantity']
+        original_cost = float(original_record['成本']["value"]) if original_record else 0
+        new_cost = original_cost + (record['quantity'] * record['price']) if record['option'] == '+' else original_cost - (record['quantity'] * record['price'])
+        values = [[
+            time.strftime("%Y/%m/%d %H:%M:%S"),
+            record['code'],
+            '買入' if record['option'] == '+' else '賣出',
+            record['option'] + str(record['quantity']),
+            record['price'],
+            record['price'] * record['quantity'],
+            original_amount,
+            new_amount,
+            original_cost,
+            new_cost,
+        ]]
+        print(values)
+        self.edit_range(spreadsheet_id, record_sheet_name, (1,0), (1,len(values[0]) - 1), values)
+
+        self.edit_cell(spreadsheet_id, overview_sheet_name, (self.row_of_ticker_symbol(spreadsheet_id, overview_sheet_name, record["code"]), original_record['股數']['index']), new_amount)
+        self.edit_cell(spreadsheet_id, overview_sheet_name, (self.row_of_ticker_symbol(spreadsheet_id, overview_sheet_name, record["code"]), original_record['成本']['index']), new_cost)
+
+
+    def get_current_records(self, spreadsheet_id, ss_name, ticker_symbol):
+        row_idx = self.row_of_ticker_symbol(spreadsheet_id, ss_name, ticker_symbol)
+        if row_idx == -1:
+            return None
+        column = self.get_range(spreadsheet_id, ss_name, (0, 0), (0, 15))[0]
+        data = self.get_range(spreadsheet_id, ss_name, (row_idx, 0), (row_idx, 15))[0]
+        data = [{"index": idx, "value": value} for idx, value in enumerate(data)]
+        record = dict(zip(column, data))
+        return record
